ceb33f5ad150bafba6baad7240c5dd1eab8f184f
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-19 15:26:41 +0500

    Reading and Writing to a strogae file

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -8,10 +8,16 @@ export class MessagesRepository {
     }
 
     async findAll() {
-
+        const contents = await readFile('message.json','utf8')
+        const messages = JSON.parse(contents)
+        return messages
     }
 
-    async create(message: string) {
-
+    async create(content: string) {
+        const contents = await readFile('messages.json','utf8')
+        const messages = JSON.parse(contents)
+        const id = Math.floor(Math.random()*999)+1
+        messages[id] = {id, content}
+        await writeFile('messages.json', JSON.stringify(messages))
     }
 }
